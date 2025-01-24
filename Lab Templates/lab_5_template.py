bb67910f3e9b38568e84f003d32515e0b94568d0
cookiebouquets <silva@bemaniso.ws>
2025-01-24 12:15:25 -0500

    made folders and added lab 5 template (WIP)

---
+++ Lab Templates/lab_5_template.py
@@ -0,0 +1,31 @@
+"""
+Name:
+Section:
+Description: Template for Lab 5
+"""
+
+"""
+Scenario: we live in a world where blackbaud no longer exists. our job is to write
+a student records program that can print out the transcript, grade level, and email of our students.
+You are to implement this using functions, dictionaries, and lists
+"""
+
+
+def getStudent(directory, student):
+    pass
+
+def getStudentGrades(directory, student):
+    pass
+
+def getStudentGradeLevel(directory,student):
+    pass
+
+def addStudent(directory):
+    pass
+
+def removeStudent(directory):
+    pass
+
+def printMenu():
+    pass
+
