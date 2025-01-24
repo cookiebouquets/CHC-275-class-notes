d1e24eee1c0818ca95df91e03a462b46ebf3c8fa
cookiebouquets <silva@bemaniso.ws>
2025-01-24 09:41:14 -0500

    Create dictionaries_sect02.py

---
+++ dictionaries_sect02.py
@@ -0,0 +1,12 @@
+"""
+    Today we are covering dictionaries.
+    
+    I'm going to try something new today,
+    it's called live coding where  i code on the screen in real time and
+    type in my notes intermittently 
+"""
+
+
+"""
+    Question:
+"""
