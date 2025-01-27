cc3e353066f56f44dddd3f722eed24657ba2d7fb
cookiebouquets <silva@bemaniso.ws>
2025-01-27 13:16:53 -0500

    Update 1_27_dictionaries_sect01.py

--- notes/1_27_dictionaries_sect01.py
+++ notes/1_27_dictionaries_sect01.py
@@ -1,3 +1,14 @@
 """
-    Today we'll be covering dictionaries
+    Today we'll be covering dictionaries. 
+    
+    From now on I'll be doing what is called live coding. Rather than making slides,
+    I will write my notes as comments in a python file so that you can see running  code alongside 
+    my annotated notes. 
+    
+    I have also made a github repo for the class. You can find all lab templates and notes there. 
+    The link was messaged to you on blackbaud. 
+"""
+
+"""
+    Some Announcements:
 """
