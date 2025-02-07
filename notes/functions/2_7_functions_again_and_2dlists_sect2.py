d0b71dcd135cd018fe2dd66df028534679d4676d
cookiebouquets <silva@bemaniso.ws>
2025-02-07 10:16:25 -0500

    restructured folders

---
+++ notes/functions/2_7_functions_again_and_2dlists_sect2.py
@@ -0,0 +1,15 @@
+""" 
+    Today we are going to finish up our conceptual understanding of functions and discuss multi-dimensional lists.
+    
+    
+        IMPORTANT: Lab 6 Release will be Feb 14, Day 2
+    
+        1. Painstaking Review of Functions again
+        2. Review of Mutability
+        3. Pass by Value vs Pass by Reference
+        4. functions philosophically example 
+        
+        5. 2D lists 
+        6. Iterating over 2D lists 
+"""
+
