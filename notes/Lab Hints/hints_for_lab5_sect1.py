5bd2d2290a5d8dfefc1249a62a644c1b5389a953
cookiebouquets <silva@bemaniso.ws>
2025-02-13 14:00:40 -0500

    Update hints_for_lab5_sect1.py

--- notes/Lab Hints/hints_for_lab5_sect1.py
+++ notes/Lab Hints/hints_for_lab5_sect1.py
@@ -59,7 +59,6 @@ print(f"var1: {var1}")
         4. make a variable called gradelevel which is the student's grade level
         5. make a variable called email which is a string for the email
         6. student = {"grades": grades, "grade level" : gradelevel, "email": email}
-        
         7. directory[studentname] = student
         
 """
@@ -122,3 +121,35 @@ print(f"var1: {var1}")
     so after forloop and add to GPA,
     return GPA/4
 """ 
+
+#FEB 13 DAY 1 (LAB IS DUE TOMORROW)
+
+
+""" 
+For juniors: Course Recommendations are live, see me if you wish to discuss next year for 277 or 280.
+
+
+277: Introduction to Computer Algorithms 
+
+    This class is basically CMSC 202 + very basic CMSC 341 at UMBC, will be taught in either Java or C++. Will cover 
+    object oriented design, asymptotic analysis (at a deeper level than this course will provide),
+    very basic data structures (linkedlists, binary trees) and algorithms (breadth first, depth first, greedy). 
+    This class will be very good for those going on to study Computer Science or any CS related field in college, 
+    you will learn a lot of very advanced topics a lot earlier than you will see them in college 
+    and will set you up for success in the difficult courses in those degree programs. 
+    
+    I'd recommend this course to the extremely motivated.
+
+280: Cybersecurity Essentials
+
+    This class is getting a redesign next year. It will basically follow this plan:
+    
+    1st semester will be all on computer networks and protocols, teaching you the language of information
+    technology. 
+    
+    2nd semester will then be on practical cybersecurity based on the concepts you learned in the first
+    semester, i.e. you need to be familiar with the vocabulary and concepts in a perfect setting
+    before you learn about what vulnerabilities are possible.
+    
+    I'd recommend this course to either Prep B guys are people looking for a lower stakes elective. 
+"""
