f47a9b912b8194dd1ed0c5d025a56e02e99af5f4
cookiebouquets <silva@bemaniso.ws>
2025-03-03 11:00:43 -0500

    Create Binary_and_Hex_sect2.py

---
+++ notes/Binary and Hex/Binary_and_Hex_sect2.py
@@ -0,0 +1,242 @@
+""" 
+     This cycle we will be covering number systems, primarily binary and hex. We will also cover
+     
+     chr() and ord() in python, and how they can be used to manipulate string data
+     
+     Lab 7 will be a smattering of problems that involve manipulating data in various number systems. 
+"""
+
+""" 
+    First thing we need to discuss is why do we care about binary at all? 
+    
+    Computers read data as 1s and 0s, the reason why this is mostly a historical electrical engineering problem
+    
+    Back in the 1940s when computers were vacuum tubes, it really only made sense to distinguish between
+    high and low voltage because the equipment back then was just straight up bad. So the equipment was larege
+    and you typically need n-1 sensing components to distinguish between n different voltage levels
+    
+    2 states is the easiest thing to distinguish because we only need 1 sensing component, if we wanted 10
+    states we would 9, so that would just miserable back then to try and distinguish states in decimal numbers
+    
+    It also turns out that binary is really efficient way to manipulate data. < we care about this a lot in CS
+    
+    
+    How exactly does binary work? The easiest way to do this is to start with decimal
+    
+    by decimal i mean base 10. In a base 10 number, each "place" corresponds to a power of 10, base 10
+    
+    5192 
+    
+    5*(10^3)+1*(10^2)+9*(10^1)+2*(10^0) <= this expression is equivalent to 5192 
+    
+    Binary numbers are pretty much the same thing, but we are working in base 2. So everything is in powers
+    of 2. 
+    
+    in base 10, the digits that we have are 0-9, if we had 10 it would be the next place over
+    
+    in binary (base 2), the digits that we have are 0,1 <= two states that correspond positionally to 
+    powers of 2
+    
+    How do we represent binary numbers? Same deal as decimal numbers, but each "place" can only be 0 or 1
+    
+    and each place corresponds to a power 2
+    
+    Let's represent 9 as a binary number 
+    
+    9 > 8 + 1 
+    
+    So we can write this as 
+    
+    1001
+    
+    The rightmost place is the 2^0 which is just 1 and the fourth position is 2^3 which is 8.  
+    
+    Some nomenclature: 
+    
+    Each individual 1 and 0 is called: bit
+    
+    bits are the smallest piece of information a computer can parse
+
+    Bits are also a completely miserable unit of measurement 
+    
+    The next unit of measurement we care about is: Byte
+    
+    How big is a byte: 8 bits
+    
+    1 bit = 1 or 0
+    1 byte = 8 bits
+    1 kilobytes = 1024 bytes 
+    1 megabyte = 1024 kilobytes
+    ...
+    
+    conversion from decimal to binary: Take our number and mod by 2, if theres a remainder put a 1 if not
+    leave it 0
+    
+    9 % 2 = 4 remainder 1 
+    
+    1
+    
+    4 % 2 = 2 remainder 0
+    
+    10
+    
+    2 % 2 = 1 remainder 0
+    
+    100 
+    
+    1 % 2 = 0 remainder 1
+    
+    1001 <= 9 which corresponds to the previous answer that we got 
+    
+    So just a review (for decimal): 
+    
+ _     _   _     _      _    _
+10^5 10^4 10^3  10^2  10^1  10^0
+
+for binary
+
+ _   _   _   _   _   _   _   _ 
+2^7 2^6 2^5 2^4 2^3 2^2 2^1 2^0
+    
+Each position corresponds to a power of 2
+
+The problem with binary is that this gets miserable incredibly quickly 
+
+
+1011 1100 1011 1010 1101 0011 <= This is a huge number and is a lot to type out. So we as genius human beings
+
+chose hexadecimal as a way to rewrite binary numbers in small amounts of space
+
+Binary = base 2
+Decimal = Base 10
+Hexadecimal = Base 16
+
+Base 16 for a reason: Biggest number you can store in 4 bits is 16, so Hex numbers represent binary numbers
+in much smaller pieces of information
+
+We have a problem: we only have 10 digits to work with 0-9. So how do we represent base 16 with only 
+10 digits? The answer: Start using letters 
+
+0 0         7 7         14 e 
+1 1         8 8         15 f
+2 2         9 9 
+3 3         10 a
+4 4         11 b
+5 5         12 c    
+6 6         13 d
+
+
+four bits is exactly 16 So we can convert binary numbers into hex numbers by just looking at 
+groupings of 4
+
+1011 1100 1011 1010 1101 0011
+b      c   b    a     d    3
+
+bcbad3
+
+^This is our hexadecimal number. We took our ginormous number and compressed it down to exactly 6 characters
+
+Converting from binary to hex is really easy,
+Converting from hex to binary is also really easy,
+
+Converting from decimal to binary is easy,
+Converting from binary to decimal, also easy
+
+Converting from decimal to hex  <= kinda hard to do off the dome. We should write algorithm for this because
+manipulating hex numbers is a pain because we're going to be dividng ginormous numbers into each other.
+
+Decimal to hex is easier than hex to decimal because we can just mod 16 and the remainder corresponds to our 
+mapping of characters (0-9,a-f). <= i'm going to leave this on the lab.
+
+So we now know that binary numbers are how information is stored on computers. How do we manipulate this 
+data in meaningful ways? We can do something called bitwise operations. 
+
+Recall from if statements, we can also augment with (AND) terms and (OR) terms. So them true/false can be
+evaluated by the individual truth values of the statements along with our AND and OR operations. We can do
+the same thing with our binary numbers. We do what is called bitwise operations and compare binary numbers
+term by term
+
+Let's start with AND 
+
+The truth table for and 
+
+1 AND 1 = 1
+1 AND 0 = 0
+0 AND 1 = 0
+0 AND 0 = 0
+
+If both things are on, we get 1, if not, we get 0 
+
+We can do the same thing for OR:
+
+1 OR 0 = 1
+0 OR 1 = 1
+1 OR 1 = 1
+0 OR 0 = 0
+
+Using these truth tables, we can construct expressions on large binary numbers
+
+    1011 1101
+OR  0101 1010
+_____________
+    1111 1111
+
+So none of them were both zeros. How about AND
+
+    1011 1101
+AND 0101 1010
+_____________
+    0001 1000
+    
+Sol we got 0001 1000 from using an AND gate bit by bit on our two numbers. The question is: How exactly
+is this useful? 
+
+Most if not all math operations are bitwise operations on binary numbers. (Look up adder/half adder
+circuits) 
+
+We can also do cryptography with bitwise operations (You'll see these on CTFs).
+
+chc and encrypt it with the key acd using XOR
+
+So i explain what XOR is. XOR stands for exclusive OR, its basically the same as OR but instead you also
+consider the 1 XOR 1 case and set that equal to 0    
+
+XOR Truth Table:
+
+1 OR 0 = 1
+0 OR 1 = 1
+1 OR 1 = 0
+0 OR 0 = 0
+
+We need XOR because OR by itself does not allow you to recover information backwards. OR is not an invertible
+function (for those in precalc/algebra 2, etc)
+
+chc and convert it to binary with this scheme
+
+1 a 5 e 9 i
+2 b 6 f 10 j
+3 c 7 g 11 k
+4 d 8 h 12 l
+    
+We can then convert CHC into a a string of decimal numbers
+
+CHC => 383 => 0011 1000 0011
+ACD => 134 => 0001 0011 0100
+
+Now what I can do is XOR CHC by ACD and get an encrypted message out of this
+
+    0011 1000 0011
+XOR 0001 0011 0100
+__________________
+    0010 1011 0111 <= our result after XOR 
+    
+Convert 0010 1011 0111 back to decimal => 2 11 7 => BKG
+
+encrypting CHC by ACD => BKG as our encrypted text
+
+This kind of encryption is called a one-time pad encryption. Next Class I will show how to decrypt this
+
+Hint: You just need to XOR again by the key.  
+    
+    
+"""
