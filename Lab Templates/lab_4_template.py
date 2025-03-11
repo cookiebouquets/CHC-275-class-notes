4e58a3942fb506c66ae28a86d0b9bf54bba6cb87
cookiebouquets <silva@bemaniso.ws>
2025-03-11 10:25:40 -0400

    Create lab_4_template.py

---
+++ Lab Templates/lab_4_template.py
@@ -0,0 +1,76 @@
+""" 
+Name:
+Section:
+Description: Lab 4 
+"""
+
+
+
+"""
+Function Name: getList
+Parameters: none
+Return Type: List
+Description: Prompts user to fill in an empty list until they are satisfied 
+"""
+def getList():
+    pass
+
+""" 
+Function Name: printMenu
+Parameters: none
+Return Type: none
+Description: Prints menu for statistics calculator
+"""
+def printMenu():
+    pass
+
+"""
+Function Name: getMean
+Parameters: List
+Return Type: Float
+Description: Calculates the mean for the list and returns the value 
+"""
+def getMean(userList):
+    pass
+
+"""
+Function Name: getMedian
+Parameters: List
+Return Type: Float
+Description: Calculates the median for the list and returns the value  
+"""
+def getMedian(userList):
+    pass
+
+""" 
+Function Name: getMin
+Parameters: List
+Return Type: Float
+Description: Finds the minimum of the unsorted list
+"""
+def getMin(userList):
+    pass
+
+""" 
+Function Name: getMax
+Parameters: List
+Return Type: Float
+Description: Finds the maximum of the unsorted list
+"""
+def getMax(userList):
+    pass
+
+""" 
+Function Name: emptyList
+Parameters: none
+Return Type: none
+Description: Empties out the list of numbers
+"""
+def emptyList():
+    pass
+
+
+
+def main():
+    pass
+
